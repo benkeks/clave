58c41505236055a445c8ae760a7ad9f46da219c2
benkeks <ben@mrkeks.net>
2022-11-20 00:11:59 +0100

    Add zoom effect for pause

--- src/main/scala/net/mrkeks/clave/game/Game.scala
+++ src/main/scala/net/mrkeks/clave/game/Game.scala
@@ -138,6 +138,11 @@ class Game(val context: DrawingContext, val input: Input, val gui: GUI, val leve
       player.foreach { p =>
         context.cameraUpdatePosition(p.getPosition)
       }
+      val newZoom = Mathf.approach(context.camera.zoom, (if (state.isInstanceOf[Paused]) .7 else 1.0), .001 * deltaTime)
+      if (newZoom != context.camera.zoom) {
+        context.camera.zoom = newZoom
+        context.camera.updateProjectionMatrix()
+      }
     }
   }
   
